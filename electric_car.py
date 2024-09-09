6d29e49c69fe075e9a1dc3fdd880849b992fde82
YjieKim <yjiekim@gmail.com>
2024-09-09 15:19:35 +0900

    Update electric_car.py

--- electric_car.py
+++ electric_car.py
@@ -8,7 +8,8 @@ import streamlit as st
 warnings.filterwarnings(action='ignore')
 
 # 한글 글꼴 설정
-plt.rc('font', family='malgun gothic')
+# plt.rc('font', family='malgun gothic')
+plt.rc('font', family='NanumGothic')
 
 # 기본 실행 함수
 def basic():
