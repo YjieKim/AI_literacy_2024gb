ceb13c23dea6a8d26091cdad68364b0c8a87f35d
YjieKim <yjiekim@gmail.com>
2024-09-15 01:15:33 +0900

    Update electric_car.py

--- electric_car.py
+++ electric_car.py
@@ -34,7 +34,7 @@ def font_setting():
         fname=r'font/NanumGothic.ttf', # ttf 파일이 저장되어 있는 경로
         name='NanumGothic')                        # 이 폰트의 원하는 이름 설정
     fm.fontManager.ttflist.insert(0, fe)              # Matplotlib에 폰트 추가
-    plt.rcParams.update({'font.size': 18, 'font.family': 'NanumGothic'}) # 폰트 설정
+    plt.rcParams.update({'font.size': 12, 'font.family': 'NanumGothic'}) # 폰트 설정
 
 
 # 기본 실행 함수
